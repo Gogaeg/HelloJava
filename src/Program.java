2db9a971af95305c31a2bd7e9e9292c3410e4b2d
Gogaeg <viktorandreevichzaycev@gmail.com>
2024-09-03 13:29:11 +0300

    задача3

--- src/Program.java
+++ src/Program.java
@@ -48,4 +48,18 @@ public class Program {
 //
 //    scanner.close();
 //}
+//    Задача№3
+//public static void main(String[] args) {
+//
+//    int multiplier = 5;
+//
+//
+//    System.out.println("Таблица умножения на " + multiplier + ":");
+//
+//
+//    for (int i = 1; i <= 10; i++) {
+//
+//        System.out.println(multiplier + " x " + i + " = " + (multiplier * i));
+//    }
 //}
+}
