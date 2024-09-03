8df5cd1d517f359aee98fa5a5ec09c380ab1a489
Gogaeg <viktorandreevichzaycev@gmail.com>
2024-09-03 13:16:46 +0300

    задача1

---
+++ src/Program.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Program {
+//    public static void main(String[] args) {
+//        System.out.println("Зайцев Виктор 4исип221");
+//    }
+//    Задача№1
+//public static void main(String[] args) {
+//
+//    Scanner scanner = new Scanner(System.in);
+//
+//
+//    System.out.print("Введите целое число: ");
+//    int number = scanner.nextInt();
+//
+//
+//    if (number % 2 == 0) {
+//        System.out.println("Число " + number + " является четным");
+//    } else {
+//        System.out.println("Число " + number + " является нечетным");
+//    }
+//
+//
+//    scanner.close();
+//}
+//}
